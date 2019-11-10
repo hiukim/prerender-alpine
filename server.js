579bb0f038a13f00ac83c139914c415bdf097593
23233 <519360498@qq.com>
2019-11-10 22:41:20 +0800

    Update server.js
    可能可以解决504的问题

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const prerender = require('prerender');
 const server = prerender({
-  chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ],
+  chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars','--shm-size=1G','--disable-dev-shm-usage' ],
   forwardHeaders: true,
   chromeLocation: '/usr/bin/chromium-browser'
 });
 
 server.use(require('prerender-request-blacklist'));
-server.use(prerender.blockResources());
+// server.use(prerender.blockResources());
 server.use(prerender.blacklist());
 server.use(prerender.httpHeaders());
 
