36aa081651a8b1683b09e4a531638a88c5941aee
23233 <519360498@qq.com>
2019-12-21 13:24:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const prerender = require('prerender');
 const server = prerender({
   chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars','--shm-size=1G','--disable-dev-shm-usage' ],
   forwardHeaders: true,
-  chromeLocation: '/usr/bin/chromium-browser'
+  chromeLocation: '/usr/bin/chromium-browser',
+  logRequests: true,
 });
 
 //server.use(require('prerender-request-blacklist'));
